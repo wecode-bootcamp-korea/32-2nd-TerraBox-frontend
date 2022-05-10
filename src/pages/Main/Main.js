4cd5e78218958a40bb50404ef5ec4949ccf62708
김병규 <byounggyukim@gimbyeong-gyus-MacBook-Pro.local>
2022-05-10 18:23:46 +0900

    [Modify]: 메인페이지 작업완료 + AWS 주소로 변환 완료+ 리뷰 반영

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Carousel from './Carousel';
 
 function Main() {
-  return <div>Main</div>;
+  const [index, setIndex] = useState(1);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://15.164.163.31:8000/movies')
+      .then(res => res.json())
+      .then(data => setMovies(data.result));
+  });
+
+  const leftClickHandler = () => {
+    return index !== 1 && setIndex(prev => prev - 1);
+  };
+
+  const rightClickHandler = () => {
+    return index !== 5 && setIndex(prev => prev + 1);
+  };
+
+  const changeBtn = input => {
+    setIndex(input);
+  };
+
+  return (
+    <MainWrapper>
+      <span>박스오피스</span>
+      <Carousel
+        index={index}
+        movies={movies}
+        leftClickHandler={leftClickHandler}
+        rightClickHandler={rightClickHandler}
+        changeBtn={changeBtn}
+      />
+    </MainWrapper>
+  );
 }
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 50px 100px;
+  background-color: rgb(21, 16, 15);
+  color: white;
+  text-align: center;
+  span {
+    padding-bottom: 10px;
+    font-size: larger;
+  }
+`;
 
 export default Main;
