4d8d9eb2d42606769658050deee428be61d92323
병규킴 <102006782+byounggyu-kim@users.noreply.github.com>
2022-07-22 15:36:09 +0900

    Add: Nav로고 드랍다운 기능 수정 및 마이페이지 레이아웃 수정

--- src/components/BookedList.js
+++ src/components/BookedList.js
@@ -2,11 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 function BookedList() {
-  return <ListBox>예약 내역</ListBox>;
+  return (
+    <ListBox>
+      <BookTitle>예약 내역</BookTitle>
+    </ListBox>
+  );
 }
 
 export default BookedList;
 
 const ListBox = styled.div`
   width: 100%;
+`;
+
+const BookTitle = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  padding-bottom: 15px;
+  border-bottom: 1px solid;
 `;
--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import MyPageBox from '../../components/MyPageBox';
 import BookedList from '../../components/BookedList';
 
 function Mypage() {
+  const navigate = useNavigate();
   const point = 1000;
-  return (
+  const conf = () => {
+    // let result = window.confirm('확인을 눌러주세요');
+    // if (result) {
+    //   alert('확인완료');
+    // } else {
+    //   alert('취소되었습니다.');
+    // }
+    navigate('/booking');
+  };
+  return localStorage.getItem('token') ? (
     <Wrapper>
       <MypageMain>
         <MyPageBox point={point} />
         <BookedList />
       </MypageMain>
     </Wrapper>
+  ) : (
+    conf()
   );
 }
 
